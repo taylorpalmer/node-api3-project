45d9b316ca799075f429045dfbda44e5dccfcdfc
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-06-09 18:47:42 -0700

    building validateUser middleware

--- index.js
+++ index.js
@@ -2,7 +2,3 @@
 const express = require("express");
 const morgan = require("morgan");
 const server = require("./server.js");
-
-server.listen(4000, () => {
-  console.log("\n* Server Running on http://localhost:4000 *\n");
-});
--- server.js
+++ server.js
@@ -1,13 +1,27 @@
-const express = require('express');
+const express = require("express");
+const logger = require("./middleware/logger");
 
 const server = express();
+const port = 4000;
 
-server.get('/', (req, res) => {
+server.use(express.json());
+
+server.use(logger("long"));
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+server.use((err, req, res, next) => {
+  console.log(err);
 
-function logger(req, res, next) {}
+  res.status(500).json({
+    message: "Something went wrong, please try again later",
+  });
+});
+
+server.listen(port, () => {
+  console.log("\n* Server Running on http://localhost:4000 *\n");
+});
 
 module.exports = server;
